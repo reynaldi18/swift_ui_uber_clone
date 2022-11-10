60e15241e1d73e5ce7e7b4e6fc3964e7c48690b0
Reynaldi Pamungkas <reynaldi.pamungkas9i@gmail.com>
2022-11-10 22:15:23 +0700

    Ride type data model

---
+++ UberSwiftUIClone/Models/RideType.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  UberSwiftUIClone
+//
+//  Created by Reynaldi Pamungkas on 10/11/22.
+//
+
+import Foundation
+
+enum RideType: Int, CaseIterable, Identifiable {
+    case uberX
+    case black
+    case uberXL
+    
+    var id: Int { return rawValue }
+    
+    var description: String {
+        switch self {
+        case .uberX: return "UberX"
+        case .black: return "UberBlack"
+        case .uberXL: return "UberXL"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .uberX: return "uber-x"
+        case .black: return "uber-black"
+        case .uberXL: return "uber-xl"
+        }
+    }
+}
