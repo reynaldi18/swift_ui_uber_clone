bf5ae4e672ce3d079cc4955a06448b160262006f
Reynaldi Pamungkas <reynaldi.pamungkas9i@gmail.com>
2022-11-30 16:08:53 +0700

    Pricing model

--- UberSwiftUIClone/Core/Trips/RideRequestView.swift
+++ UberSwiftUIClone/Core/Trips/RideRequestView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct RideRequestView: View {
     @State private var selectedRideType: RideType = .uberX
+    @EnvironmentObject var locationViewModel: LocationSearchViewModel
+    
     var body: some View {
         VStack {
             Capsule()
@@ -76,7 +78,7 @@ struct RideRequestView: View {
                             VStack(alignment: .leading, spacing: 4) {
                                 Text(type.description)
                                     .font(.system(size: 14, weight: .semibold))
-                                Text("$22.04")
+                                Text(locationViewModel.computeRidePrice(forType: type).toCurrency())
                                     .font(.system(size: 14, weight: .semibold))
                             }
                             .padding()
---
+++ UberSwiftUIClone/Extensions/Double.swift
@@ -0,0 +1,22 @@
+//
+//  Double.swift
+//  UberSwiftUIClone
+//
+//  Created by Reynaldi Pamungkas on 30/11/22.
+//
+
+import Foundation
+
+extension Double {
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    func toCurrency() -> String {
+        return currencyFormatter.string(for: self) ?? ""
+    }
+}
